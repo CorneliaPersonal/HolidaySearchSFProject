09b70b6494ab02f8e76696c0c07bc24790930c00
cornelia.s <cornelia.s@bsky.co.za>
2024-08-19 23:09:05 +0200

    Commit lwc changes

--- force-app/main/default/lwc/search_by_ID_Number/search_by_ID_Number.js
+++ force-app/main/default/lwc/search_by_ID_Number/search_by_ID_Number.js
@@ -1,17 +1,44 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Search_by_ID_Number extends LightningElement {
 
     @track showSpinner = false;
-    @track disableSearch = true;
+    @track isSearchDisabled = true;
+    @track idNumber = '';
+    @track showValidationText = false;
 
-    handleIDNumberInput() {
-        
+    handleInputChange(event) {
+        this.idNumber = event.target.value;
+
+        if((this.idNumber.length === 13) && (/^\d+$/.test(idNumber))){
+            this.showValidationText = false;
+            this.isSearchDisabled = idValidationLuhn(idNumber);
+        } 
+        else {
+            this.showValidationText = true;
+        }
     }
 
     handleSearch() {
         this.showSpinner = true;
 
+    }
+
+    idValidationLuhn(idNumber) {
+        let sum = 0;
+
+        for (let i = 0; i < idNumber.length; i++) {
+            let currentNumber = parseInt(idNumber[i]);
+
+            if (i % 2 === 0) {
+                currentNumber *= 2;
+                if (currentNumber > 9) {
+                    currentNumber -= 9;
+                }
+            }
+            sum += currentNumber;
+        }
+        return sum % 10 === 0;
     }
 
 
