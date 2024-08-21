d784f446782834f2099f3125b4a0fe443e2b3b2d
cornelia.s <cornelia.s@bsky.co.za>
2024-08-21 21:02:16 +0200

    21/08 21h02

--- force-app/main/default/lwc/idSearch/idSearch.js
+++ force-app/main/default/lwc/idSearch/idSearch.js
@@ -1,7 +1,7 @@
 import { LightningElement, track } from 'lwc';
 import { leftToRightLuhnChecksum } from 'c/luhnChecksum';
 import processIDAndExtractYear from '@salesforce/apex/IDSearchController.processIDAndExtractYear';
-//import executeCallout from '@salesforce/apex/IDSearchApiService.executeCallout';
+import executeCallout from '@salesforce/apex/IDSearchApiService.executeCallout';
 
 export default class Search_by_ID_Number extends LightningElement {
 
@@ -29,15 +29,23 @@ export default class Search_by_ID_Number extends LightningElement {
 
     handleSearch() {
         this.showSpinner = true;
+        let countryCode = 'ZA';
+        let holidayType = 'national';
+        let year = '';
 
         processIDAndExtractYear({ idNumber: this.idNumber })
         .then(result => {
-            this.showSpinner = false;
-            let year = result;
+            year = result;
             console.log(`Year: ${year}`);
+            return executeCallout({ year: year, countryCode: countryCode, holidayType: holidayType });
+        })
+        .then(response => {
+            console.log(`Holidays: ${response}`);
+            this.showSpinner = false;
         })
         .catch(error => {
             console.log(JSON.stringify(error));
+            this.showSpinner = false;
         })
     }
 }
