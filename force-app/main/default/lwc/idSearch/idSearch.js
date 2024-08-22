586205dfd1798c16774dae609ea1b0fa15e8e2ce
cornelia.s <cornelia.s@bsky.co.za>
2024-08-22 02:45:59 +0200

    22/08 first

--- force-app/main/default/lwc/idSearch/idSearch.js
+++ force-app/main/default/lwc/idSearch/idSearch.js
@@ -1,51 +1,88 @@
-import { LightningElement, track } from 'lwc';
-import { leftToRightLuhnChecksum } from 'c/luhnChecksum';
-import processIDAndExtractYear from '@salesforce/apex/IDSearchController.processIDAndExtractYear';
-import executeCallout from '@salesforce/apex/IDSearchApiService.executeCallout';
+import { LightningElement } from "lwc";
+import { leftToRightLuhnChecksum } from "c/luhnChecksum";
+import processIDAndExtractYear from "@salesforce/apex/IDSearchController.processIDAndExtractYear";
+import executeCallout from "@salesforce/apex/IDSearchApiService.executeCallout";
+import BEACH_IMAGE from "@salesforce/resourceUrl/Beach_png";
 
 export default class Search_by_ID_Number extends LightningElement {
+  columns = [
+    { label: "Name", fieldName: "name" },
+    { label: "Date", fieldName: "date" }
+  ];
+  data = [];
+  showSpinner = false;
+  isSearchDisabled = true;
+  idNumber = "";
+  showValidationText = false;
+  validationMessage = "Please input a valid 13-digit ID number.";
+  noHolidays = false;
+  noHolidaysMessage = "Something went wrong, please try again later";
+  imageUrl = BEACH_IMAGE;
 
-    @track showSpinner = false;
-    @track isSearchDisabled = true;
-    @track idNumber = '';
-    @track showValidationText = false;
+  connectedCallback() {}
 
-    connectedCallback() {
+  handleInputChange(event) {
+    this.idNumber = event.target.value;
+
+    if (!/^\d+$/.test(this.idNumber)) {
+      this.showValidationText = true;
+      this.isSearchDisabled = true;
+    } else if (this.idNumber.length === 13) {
+      this.isSearchDisabled = !leftToRightLuhnChecksum(this.idNumber);
+      this.showValidationText = this.isSearchDisabled;
     }
+  }
 
-    handleInputChange(event) {
-        this.idNumber = event.target.value;
+  handleSearch() {
+    this.showSpinner = true;
+    let countryCode = "ZA";
+    let holidayType = "national";
+    let year = "";
 
-        if((this.idNumber.length === 13) && (/^\d+$/.test(this.idNumber))){
-            this.showValidationText = false;
-            this.isSearchDisabled = !leftToRightLuhnChecksum(this.idNumber);
-            console.log(this.idNumber);
-        } 
-        else {
-            this.showValidationText = true;
-            this.isSearchDisabled = true;
+    processIDAndExtractYear({ idNumber: this.idNumber })
+      .then((result) => {
+        year = result;
+        return executeCallout({
+          year: year,
+          countryCode: countryCode,
+          holidayType: holidayType
+        });
+      })
+      .then((response) => {
+        let parsedResponse;
+        try {
+          parsedResponse = JSON.parse(response);
+        } catch (error) {
+          console.error("Error parsing response:", error);
+          this.showSpinner = false;
+          this.noHolidays = true;
+          this.data = [];
+          return;
         }
-    }
+        let holidays =
+          parsedResponse.response &&
+          Array.isArray(parsedResponse.response.holidays)
+            ? parsedResponse.response.holidays
+            : [];
 
-    handleSearch() {
-        this.showSpinner = true;
-        let countryCode = 'ZA';
-        let holidayType = 'national';
-        let year = '';
-
-        processIDAndExtractYear({ idNumber: this.idNumber })
-        .then(result => {
-            year = result;
-            console.log(`Year: ${year}`);
-            return executeCallout({ year: year, countryCode: countryCode, holidayType: holidayType });
-        })
-        .then(response => {
-            console.log(`Holidays: ${response}`);
-            this.showSpinner = false;
-        })
-        .catch(error => {
-            console.log(JSON.stringify(error));
-            this.showSpinner = false;
-        })
-    }
+        if (holidays.length === 0) {
+          this.noHolidays = true;
+          this.data = [];
+        } else {
+          this.noHolidays = false;
+          this.data = holidays.map((holiday) => {
+            return {
+              name: holiday.name,
+              date: holiday.date.iso
+            };
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        this.showSpinner = false;
+        this.noHolidays = true;
+      });
+    this.showSpinner = false;
+  }
 }
